5519b0fdfef6d01a5c727f3203c13561c2d85ce7
mewhooik <sujalchavdaa@gmail.com>
2026-05-02 23:02:15 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "22370234"
+api_hash = "706badded011715ae115e5ab3bf83f87"
+bot_token = "8666609283:AAEbyMAV_UHsv-nsqDllvdrSZGEee718F3c"
+auth_users = [8545564031]
+sudo_users = [8545564031]
